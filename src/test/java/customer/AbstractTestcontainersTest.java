babbdcf86c7cdf4ad1cc140f610f90cd1e716c52
Ruben Becerra Cortes <rubenbc04@gmail.com>
2026-08-23 19:03:00 +0200

    fix: provide redis testcontainer in AbstractTestcontainersTest

--- src/test/java/customer/AbstractTestcontainersTest.java
+++ src/test/java/customer/AbstractTestcontainersTest.java
@@ -1,19 +1,37 @@
 package customer;
 
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractTestcontainersTest {
 
     @Container
-    @ServiceConnection
-    protected static final PostgreSQLContainer<?> postgres =
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
             new PostgreSQLContainer<>("postgres:16-alpine")
                     .withDatabaseName("customers_test_db")
-                    .withUsername("test_user")
-                    .withPassword("test_password");
+                    .withUsername("admin")
+                    .withPassword("password123");
 
+    @Container
+    protected static final GenericContainer<?> redisContainer =
+            new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+                    .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    private static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
+    }
 }
